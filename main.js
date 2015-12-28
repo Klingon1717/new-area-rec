39a6616cefb003d57d5c48d4fbf60bb29f4f7f4a
Whitney Murray <wnksmith92@yahoo.com>
2015-12-28 15:04:53 -0600

    fixed space issue

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var feet = length * width;
 var metric = feet * .09290304;
 var areaFt = feet;
 var areaM = metric;
-var area = areaFt + 'square feet' + ' '+ areaM + 'meters';
+var area = areaFt + ' ' + 'square feet' + ' '+ areaM + ' ' + 'meters';
 
 if(typeof areaFt === "number"){
 	console.log(area);
